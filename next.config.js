e477c6daba0dfd62193c05df411f83db688ae1fd
germanny <germanny@gmail.com>
2024-03-17 16:52:47 -0500

    fix next.config again

--- next.config.js
+++ next.config.js
@@ -4,21 +4,12 @@
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
-
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
-
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
-
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
 };
 
 module.exports = nextConfig;
